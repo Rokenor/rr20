9cb2a13937ce318b6ff1f8e2cd0f9392d1254b01
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2020-01-08 20:48:54 +0200

    weather

--- src/index.js
+++ src/index.js
@@ -2,15 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const buttonText = 'Click me!';
-    const labelText = 'Enter name:';
-
     return (
-    <div>
-        <label className="label" htmlFor="name">{labelText}</label>
-        <input id="name" type="text" />
-        <button style={{ backgroundColor: "blue", color: "white" }}>{buttonText}</button>
-    </div>
+        <div>Hi there!</div>
     );
 };
 
