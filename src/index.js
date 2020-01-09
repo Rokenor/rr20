aaaa2576698ae9a5e9099476f8c600e7f27dcb56
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2020-01-09 16:26:43 +0200

    weather

--- src/index.js
+++ src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.error(err)
-        );
+    constructor(props) {
+        super(props);
 
-        return <div>Latitude: </div>
+        this.state = {
+            lat: null,
+            errorMessage: ""
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => {
+                this.setState({ errorMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        if (!this.state.lat && this.state.errorMessage === "") {
+            return <div>Loading...</div>
+        }
+
+        if (!this.state.lat && this.state.errorMessage !== "") {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        return (
+            <div>Latitude: {this.state.lat}</div>
+        );
     }
 }
 
