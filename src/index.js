f00bb39be39ae7b0fbccb5feda3b64766ed583ff
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-01-09 22:23:36 +0200

    pics init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
-class App extends React.Component {
-    
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <App />, 
+    document.querySelector("#root")
+);
