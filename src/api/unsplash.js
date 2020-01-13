34c8e3b51a5e25cb92d78bdeaa0e6afb38588f80
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2020-01-13 14:45:50 +0200

    videos: init

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID 690e7dc9ff115dddbd499b0124c8da120410ea4857d2d15bcecf15780835eba2'
-    }
-});
