ede4d416d5f8e7b3bfed728c40834a0d89dcda87
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2020-01-13 17:42:31 +0200

    videos: videos list

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,8 +6,9 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
+    type: 'video',
     maxResult: 5,
-    key: KEY
+    key: `${KEY}`
   }
 });
 
