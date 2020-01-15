a4307a8da77ef75331222c3b92d850e690106352
Oleksandr Kovrizhnykh <rokenor@zeoalliance.com>
2020-01-15 21:00:14 +0200

    blog: memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,23 @@
+import _ from 'lodash';
 import jsonPlaseholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaseholder.get(`https://jsonplaceholder.typicode.com/posts`);
     
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ 
+      type: 'FETCH_POSTS', 
+      payload: response.data 
+    });
   }
 };
+
+export const fetchUser = (id) => (dispatch) => {
+  _fetchUser(id, dispatch);
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaseholder.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER',  payload: response.data });
+});
