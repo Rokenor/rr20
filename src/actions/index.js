3464e4d9f97a6d3167e05ffe7ce4ba28c0e34a90
Alexandr Kovrizhnykh <a.kovrizhnykh@gmail.com>
2020-01-16 22:48:05 +0200

    clear

--- src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import jsonPlaseholder from '../apis/jsonPlaceholder';
-
-export const fetchPosts = () => {
-  return async (dispatch) => {
-    const response = await jsonPlaseholder.get(`https://jsonplaceholder.typicode.com/posts`);
-    
-    dispatch({ type: 'FETCH_POSTS', payload: response });
-  }
-};
